7f5cb5c60dd58b940607dc0f68e4c3e64ec201bb
rishu-m <rishumishrmau510@gmail.com>
2026-06-06 12:46:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (10.75%) - LeetSync

---
+++ 69-sqrtx/sqrtx.c
@@ -0,0 +1,18 @@
+int mySqrt(int x) {
+    if (x == 0 || x == 1) return x;
+
+    int left = 1, right = x / 2, ans = 0;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (mid <= x / mid) {   // avoid overflow by using division
+            ans = mid;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return ans;
+}
